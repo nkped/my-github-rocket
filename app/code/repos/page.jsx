6bc5eede31789c0dc2fb1fdb8ab7462f7f76e6bb
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-08-31 19:23:54 +0200

    init commit

---
+++ app/code/repos/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Repospage = () => {
+  return (
+    <div>Repospage</div>
+  )
+}
+
+export default Repospage
---
+++ app/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <div><Link href='/code/repos'>Repos</Link></div>
+  )
+}
+
+export default Header
