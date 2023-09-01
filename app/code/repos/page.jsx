8aae652feac4d75fb88b698a19aefb333a4248a0
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-09-01 13:41:19 +0200

    add repopage

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import Link from 'next/link'
+import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa'
 
 
+async function fetchRepos() {
+    const response = await fetch('https://api.github.com/users/nkped/repos')
+    const repos = await response.json()
 
-const Repospage = () => {
+    return repos
+}
+
+
+const Repospage = async () => {
+    const repos = await fetchRepos()
 
     return (
-        <div>
+        <div className="container">
+            <h2>Repositories</h2>
+            <ul className="repo-list">
+                {repos.map((repo) => (
+                    <li key={repo.id}>
+                        <Link href={`/code/repos/${repo.name}`}>
+
+                        <h3>{repo.name}</h3>
+                        <p>{repo.description}</p>
+                        <div className="repo-details">
+                            <span><FaStar />{repo.stargazers_count}</span>
+                            <span><FaCodeBranch />{repo.forks_count}</span>
+                            <span><FaEye />{repo.watchers_count}</span>
+                        </div>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )   
 }
