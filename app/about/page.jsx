ed9a796436ecaeacda0cab8f2c97777322562c12
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-09-01 12:02:02 +0200

    file removed

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+const About = () => {
+  return (
+    <div><h1>About</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quaerat fugiat earum natus fuga laudantium ipsum laborum dolores ea, nihil voluptates obcaecati eius consequatur reprehenderit, porro, minima odio iusto? Pariatur!</p></div>
+  )
+}
+
+export default About
---
+++ app/about/team/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Teampage = () => {
+  return (
+    <div><h1>Teampage</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi soluta exercitationem perspiciatis delectus inventore voluptatum qui unde aliquam ex saepe.</p></div>
+  )
+}
+
+export default Teampage
