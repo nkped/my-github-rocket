a25686a84c0bf05dfbd2e5f7428110b9e0138e01
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-09-01 13:50:36 +0200

    add loader - animation not working

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -5,6 +5,8 @@ import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa'
 
 async function fetchRepos() {
     const response = await fetch('https://api.github.com/users/nkped/repos')
+
+    await new Promise((resolve) => setTimeout(resolve, 2000))
     const repos = await response.json()
 
     return repos
---
+++ app/loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const LoadingPage = () => {
+    return (
+      <div className='loader'>
+        <div className='spinner'></div>
+      </div>
+    );
+  };
+  export default LoadingPage;
